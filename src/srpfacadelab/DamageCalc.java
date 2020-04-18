ac8a9239aab1e0b0200ec56da2086365c35244ce
[Shreya Tumma] <ShreyaTumma@users.noreply.github.com>
2020-04-18 00:29:15 -0700

    DamageCalc updated

--- src/srpfacadelab/DamageCalc.java
+++ src/srpfacadelab/DamageCalc.java
@@ -8,17 +8,17 @@ public class DamageCalc {
     }
     
     public void takeDamage(int damage) {
-        if (player.calculateInventoryWeight() < (player.getCarryingCapacity() / 2)) {
-            player.setDamage((player.getDamage() - (int)(player.getDamage() * 0.25)));
-        }
+        int damageToDeal = damage - player.getArmour();
         
+        if (player.calculateInventoryWeight() < (player.getCarryingCapacity() / 2)) {
+            damageToDeal = damageToDeal - (int)(damageToDeal * .25);
+        }
+
         if (damage < player.getArmour()) {
             player.getGameEngine().playSpecialEffect("parry");
         }
 
-        int damageToDeal = damage - player.getArmour();
-        player.setHealth(player.getHealth()- damageToDeal);
-
+        player.setHealth(player.getHealth() - damageToDeal);
         player.getGameEngine().playSpecialEffect("lots_of_gore");
     }
 }
